2e3cc45c0b818a31ab11e622c1c6eb7fa9ba4038
longbits <2316570512@qq.com>
2018-10-31 19:28:47 +0800

    补充次级新闻组件

--- weiboFresh/miniprogram/components/subNews/index.js
+++ weiboFresh/miniprogram/components/subNews/index.js
@@ -1,4 +1,4 @@
-// components/newsBox/index.js
+// components/subNews/index.js
 Component({
   /**
    * 组件的属性列表
@@ -18,10 +18,6 @@ Component({
    * 组件的方法列表
    */
   methods: {
-    goInner: () => {
-      wx.navigateTo({
-        url: '../../pages/inner/inner'
-      })
-    }
+
   }
 })
