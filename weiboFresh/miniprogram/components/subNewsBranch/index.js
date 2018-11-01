20c8448af924b226adcfd1c05422305492d1b774
longbits <2316570512@qq.com>
2018-11-01 18:04:50 +0800

    增加评论组件

--- weiboFresh/miniprogram/components/subNewsBranch/index.js
+++ weiboFresh/miniprogram/components/subNewsBranch/index.js
@@ -17,13 +17,18 @@ Component({
    * 组件的方法列表
    */
   methods: {
-    //一个坑：这里如果用箭头函数的话this是满足不了你的要求的！
+    //一个坑：这里如果用箭头函数的话this也就是toggleText对象,他是调用不了this.setData，故采用function！
     toggleText: function(e) {
       // console.log(e.currentTarget.dataset.hi);
       let isFolded = !this.data.isFolded;
       this.setData({
         isFolded
     })
+    },
+    goDetail: function() {
+      wx.navigateTo({
+        url: '/pages/detail/detail'
+      })
     }
   }
 })
