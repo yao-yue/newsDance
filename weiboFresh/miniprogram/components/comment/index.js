4d338cff3f39e953d2b875db9a9fbab1fca8a3d2
longbits <2316570512@qq.com>
2018-11-07 22:49:42 +0800

    优化点赞!!!  非常开心

--- weiboFresh/miniprogram/components/comment/index.js
+++ weiboFresh/miniprogram/components/comment/index.js
@@ -15,6 +15,8 @@ Component({
    */
   data: {
     doneList: [],
+    likeNumList: [],
+    likeAdd: 10,
   },
 
   /**
@@ -28,8 +30,10 @@ Component({
     },
 
     likeAdd: function(e) {
+      var that = this;
       let id = e.currentTarget.dataset.hi._id;
       let idx = e.currentTarget.dataset.idx;
+      let likeNumList = that.data.likeNumList;
       console.log(id)
       wx.cloud.callFunction({
         name: 'like',
@@ -37,21 +41,35 @@ Component({
           a: id 
         }
       }).then(res => {   
-        for(let i = 0; i< this.data.comments.length; i++)
-        {
-          if (i == idx) {
-            var str = 'comments['+idx+'].likeNum'
-            var doneList = 'doneList['+idx+']'
-            this.setData({
-              [str]:res.result.data.likeNum,
-              [doneList]: true
-            })
-          }
-        } 
+        //这里提供两种思路：
+        // 1. 更改comments里面具体likeNum字段进行刷新
+        // 2. 用一个数组来存放更新的数据，如果数字的索引位置被赋值，则页面直接显示这个更新的数字，也是异曲同工之妙。
+        //   因为用户关心的是数据的变化，我们可以先把数据的变化产生，至于数据的内部变化让他异步慢慢的去做
+        // for(let i = 0; i< that.data.comments.length; i++)
+        // {
+        //   if (i == idx) {
+        //     var str = 'comments['+idx+'].likeNum'
+        //     var doneList = 'doneList['+idx+']'
+        //     that.setData({
+        //       [str]:res.result.data.likeNum,
+        //       [doneList]: true,
+        //     })
+        //     console.log(likeNumList[idx])
+        //   }
+        // } 
       }).catch(err => {
         console.log(err)
       })
 
+      // 性能得到爆炸性的提升！！！ 真是令人开心
+      likeNumList[idx] = (that.data.comments[idx].likeNum + that.data.likeAdd);
+      var doneList = 'doneList['+idx+']'
+      that.setData({
+        likeNumList,
+        [doneList]: true,
+        likeAdd: that.data.likeAdd+10
+      })
+      
     }
   }
 })
